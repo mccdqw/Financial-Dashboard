aae2dd64c0d32e494cf29d4e054f4cd4a7d5199b
Matthew Connealy <mccdqw@mail.missouri.edu>
2023-02-20 22:51:34 -0600

    refactor implementations and have cleaner output data

--- Efficient Frontier/efficientFrontier.py
+++ Efficient Frontier/efficientFrontier.py
@@ -4,97 +4,104 @@ import yfinance as yf
 import scipy.optimize as sc
 
 
-# get data
 def getData(stocks, start, end):
     stockData = yf.download(stocks, start=start, end=end)
-    stockData = stockData['Close']
-    
+    stockData = stockData['Adj Close']
     returns = stockData.pct_change()
     meanReturns = returns.mean()
     covMatrix = returns.cov()
     return meanReturns, covMatrix
 
-def portfolioPerformance(weights, meanReturns, covMatrix):
-    returns = np.sum(meanReturns * weights) * 252
-    std = np.sqrt(np.dot(weights.T, np.dot(covMatrix, weights))) * np.sqrt(252)
-    return returns, std
+def portfolioPerformance(weights, meanReturns, covMatrix, riskFreeRate):
+    returns = np.sum(meanReturns*weights)*252
+    std = np.sqrt(np.dot(weights.T,np.dot(covMatrix, weights)))*np.sqrt(252)
+    sharpe = (returns - riskFreeRate) / std
+    return returns, std, sharpe
 
-'''
-    In order to maximize the sharpe ratio, we can minimize
-    the negative sharpe ratio.
-'''
-def negativeSharpRatio(weights, meanReturns, covMatrix, riskFreeRate=0):
-    pReturns, pStd = portfolioPerformance(weights, meanReturns, covMatrix)
-    return -(pReturns - riskFreeRate) / pStd
+def negativeSharpeRatio(weights, meanReturns, covMatrix, riskFreeRate = 0):
+    pReturns, pStd, pSharpe = portfolioPerformance(weights, meanReturns, covMatrix, riskFreeRate)
+    return -pSharpe
 
 def maxSharpeRatio(meanReturns, covMatrix, riskFreeRate = 0, constraintSet=(0,1)):
-    # Minimize the negative sharpe ratio by altering the weights of the portfolio
+    "Minimize the negative SR, by altering the weights of the portfolio"
     numAssets = len(meanReturns)
     args = (meanReturns, covMatrix, riskFreeRate)
-
-    # all of the summations of the weights in the portfolio have to
-    # add up to 1
-    constraints = ({ 'type': 'eq', 'fun': lambda x: np.sum(x) - 1})
+    constraints = ({'type': 'eq', 'fun': lambda x: np.sum(x) - 1})
     bound = constraintSet
     bounds = tuple(bound for asset in range(numAssets))
-
-    # first guess is equal distribution
-    result = sc.minimize(negativeSharpRatio, numAssets * [1./numAssets], args=args,
-                        method = 'SLSQP', bounds=bounds, constraints=constraints)
+    result = sc.minimize(negativeSharpeRatio, numAssets*[1./numAssets], args=args, method='SLSQP', bounds=bounds, constraints=constraints)
     return result
 
-def maxSortinoRatio(meanReturns, riskFreeRate):
-    # function to calculate the sortino ratio
-    def negativeSortinoRatio(weights):
-        portfolio_return = np.dot(meanReturns, weights)
-        negative_returns = np.minimum(returns - portfolio_return, 0)
-        downside_deviation = np.sqrt(np.mean(negative_returns ** 2))
-        
-        return -(portfolio_return - riskFreeRate) / downside_deviation
+def getData2(stocks, start, end):
+    stockData = yf.download(stocks, start=start, end=end)
+    stockData = stockData['Close']
+    returns = stockData.pct_change()
+    meanReturns = returns.mean()
+    covMatrix = returns.cov()
+    return meanReturns, covMatrix
 
+# Define the function to calculate portfolio performance
+def portfolioPerformance2(weights, meanReturns, covMatrix):
+    returns = np.sum(meanReturns * weights) * 252
+    std = np.sqrt(
+        np.dot(weights.T, np.dot(covMatrix, weights))
+    ) * np.sqrt(252)
+    return returns, std
+
+# Define the function to calculate negative Sortino ratio
+def negativeSortinoRatio(weights, meanReturns, covMatrix, riskFreeRate=0.0, mar=0.0):
+    pReturns, pStd = portfolioPerformance2(weights, meanReturns, covMatrix)
+    downsideDeviation = np.sqrt(np.dot(weights.T, np.dot(covMatrix.loc[meanReturns < mar, meanReturns < mar], weights))) * np.sqrt(252)
+    return -(pReturns - riskFreeRate) / downsideDeviation
+
+# Define the function to maximize Sortino ratio
+def maxSortinoRatio(meanReturns, covMatrix, riskFreeRate=0.0, mar=0.0, constraintSet=(0,1)):
     numAssets = len(meanReturns)
-    initial_weights = np.ones(numAssets) / numAssets
-    
-    # define the optimization constraints (weights must sum to 1)
-    constraints = [{'type': 'eq', 'fun': lambda weights: np.sum(weights) - 1}]
-    
-    # define the bounds on the weights (between 0-1)
-    bounds = [(0,1)] * numAssets
-
-    result = sc.minimize(negativeSortinoRatio, initial_weights,
-                        method = 'SLSQP', bounds=bounds, constraints=constraints)
+    args = (meanReturns, covMatrix, riskFreeRate, mar)
+    constraints = ({'type': 'eq', 'fun': lambda x: np.sum(x) - 1})
+    bounds = tuple(constraintSet for asset in range(numAssets))
+    result = sc.minimize(negativeSortinoRatio, numAssets*[1./numAssets], args=args, method='SLSQP', bounds=bounds, constraints=constraints)
     return result
 
-stockList = ['AAPL', 'WMT', 'HD', 'AMZN']
-endDate = dt.datetime.now()
-startDate = endDate - dt.timedelta(days=365)
+stockList = ['AAPL', 'MSFT', 'JNJ']
+endDate = '2022-12-31'
+startDate = '2022-01-01'
 
-weights = np.array([0.4, 0.3, 0.2, 0.1])
+weights = np.array([0.3, 0.5, 0.2])
 
 '''
     Sharpe Ratio Calculation
 '''
-
-
-meanReturns, covMatrix = getData(stockList, start=startDate, end=endDate)
-returns, std = portfolioPerformance(weights, meanReturns, covMatrix)
-
-sharpeResult = maxSharpeRatio(meanReturns, covMatrix)
-maxSharpeRatio, maxWeights = sharpeResult['fun'], sharpeResult['x']
-
+# Example usage
+meanReturns, covMatrix = getData(['AAPL', 'WMT', 'HD', 'AMZN'], '2021-01-01', '2022-01-01')
+result = maxSharpeRatio(meanReturns, covMatrix, riskFreeRate=0)
+print("Optimal weights: ", result.x)
+print("Sharpe ratio: ", -result.fun)
 
 
 '''
     Sortino Ratio Calculation
 '''
 
-meanReturns, covMatrix = getData(stockList, start=startDate, end=endDate)
-returns, std = portfolioPerformance(weights, meanReturns, covMatrix)
 
-sortinoResult = maxSortinoRatio(meanReturns, 0)
-maxSortinoRatio, maxWeights = sortinoResult['fun'], sortinoResult['x']
+meanReturns, covMatrix = getData2(stockList, start=startDate, end=endDate)
+mar = 0.001
+result = maxSortinoRatio(meanReturns, covMatrix, mar=mar)
+weights = result.x
+pReturns, pStd = portfolioPerformance2(weights, meanReturns, covMatrix)
+downsideReturns = meanReturns[meanReturns < mar]
+
+if len(downsideReturns) == 0:
+    downsideDeviation = 0
+else:
+    downsideDeviation = np.sqrt(np.dot(weights.T, np.dot(covMatrix.loc[meanReturns < mar, meanReturns < mar], weights))) * np.sqrt(252)
+sortinoRatio = (pReturns - mar) / downsideDeviation
 
 
-print(maxSharpeRatio, maxWeights)
-print(maxSortinoRatio, maxWeights)
+
+#print(maxSharpeRatio, maxWeights)
+print("Weights:", weights)
+print("Portfolio returns:", pReturns)
+print("Portfolio downside deviation:", downsideDeviation)
+print("Sortino ratio:", sortinoRatio)
 
